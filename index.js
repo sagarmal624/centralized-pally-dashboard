a58e2721be2c3d263da0e3b4ddc560a5bbeee192
Sagar Shankhala <sagshank@publicisgroupe.net>
2020-11-12 18:24:34 +0530

    added project

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var server = express();
 server.set("view engine","vash")
 server.get('/', function (req, res) {
     var market=process.env.MARKETS || 'Pally'
-    res.render('index', { markets: market.split(","),image: process.env.IMAGE});
+    res.render('index', { markets: market.split(","),image: process.env.IMAGE,url:process.env.PALLY_BACKEND_URL});
 });
 server.listen(process.env.PORT);
